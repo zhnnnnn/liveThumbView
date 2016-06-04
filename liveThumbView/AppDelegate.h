9e1cebcba5de477ecf22c1a7c892abdfd48cf7c0
zhnnnnn <475816908@qq.com>
2016-06-04 21:51:04 +0800

    Initial Commit

---
+++ liveThumbView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  liveThumbView
+//
+//  Created by zhn on 16/6/4.
+//  Copyright © 2016年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ liveThumbView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  liveThumbView
+//
+//  Created by zhn on 16/6/4.
+//  Copyright © 2016年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
